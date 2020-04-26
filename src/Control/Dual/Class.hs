9bc06b8936cf13d227175f15ce0e0422d47d6f41
Gabriel Volpe <volpegabriel@gmail.com>
2020-04-26 16:52:06 +0200

    version bounds + documentation

--- src/Control/Dual/Class.hs
+++ src/Control/Dual/Class.hs
@@ -1,5 +1,15 @@
 {-# LANGUAGE FunctionalDependencies, TypeOperators #-}
 
+{-|
+Module      : Control.Dual.Class
+Description : Definition of the 'Dual' class and its functions.
+Copyright   : (c) Gabriel Volpe, 2020
+License     : Apache-2.0
+Maintainer  : volpegabriel'gmail.com
+Stability   : experimental
+
+You can find here functions such as 'parMap2', 'parTraverse', 'parBitraverse', etc.
+-}
 module Control.Dual.Class where
 
 import           Control.Applicative            ( ZipList(..) )
@@ -16,33 +26,37 @@ import           Data.Validation                ( Validation
                                                 , fromEither
                                                 )
 
-{-
- - The @Dual@ class abstracts over @Monad@s that have a dual
- - @Applicative@ instance that acts in a different useful way.
- -
- - E.g., the duality between @Either@ and @Validation@. As well
- - as the duality between @IO@ and @Concurrently@.
- -
- - It can also be seen as an isomorphism defined at the class level.
- -}
+{- | The Dual class abstracts over 'Monad's that have a dual
+'Applicative' instance that acts in a different useful way.
+
+E.g., the duality between 'Either' and 'Validation'. As well
+as the duality between 'IO' and 'Concurrently'.
+
+It can also be seen as an isomorphism defined at the class level.
+-}
 class (Monad m, Applicative f) => Dual f m | m -> f, f -> m where
+  {- | A natural transformation from 'm' to 'f'
+  -}
   parallel :: m :~> f
+
+  {- | A natural transformation from 'f' to 'm'
+  -}
   sequential :: f :~> m
 
-  {-
-  It is the analogue to using @<$>@ and @<*>@ for the dual
-  @Applicative@ of the current @Monad@, as defined by the
-  relationship defined by the @Dual@ instance.
+  {- |
+  It is the analogue to using '<$>' and '<*>' for the dual
+  'Applicative' of the current 'Monad', as defined by the
+  relationship defined by the 'Dual' instance.
   -}
   parMap2 :: m a0 -> m a1 -> (a0 -> a1 -> a) -> m a
   parMap2 ma0 ma1 f = (#) sequential $ f
     <$> (#) parallel ma0
     <*> (#) parallel ma1
 
-  {-
-  It is the analogue to using @<$>@ and @<*>@ for the dual
-  @Applicative@ of the current @Monad@, as defined by the
-  relationship defined by the @Dual@ instance.
+  {- |
+  It is the analogue to using '<$>' and '<*>' for the dual
+  'Applicative' of the current 'Monad', as defined by the
+  relationship defined by the 'Dual' instance.
   -}
   parMap3 :: m a0 -> m a1 -> m a2 -> (a0 -> a1 -> a2 -> a) -> m a
   parMap3 ma0 ma1 ma2 f = (#) sequential $ f
@@ -50,10 +64,10 @@ class (Monad m, Applicative f) => Dual f m | m -> f, f -> m where
     <*> (#) parallel ma1
     <*> (#) parallel ma2
 
-  {-
-  It is the analogue to using @<$>@ and @<*>@ for the dual
-  @Applicative@ of the current @Monad@, as defined by the
-  relationship defined by the @Dual@ instance.
+  {- |
+  It is the analogue to using '<$>' and '<*>' for the dual
+  'Applicative' of the current 'Monad', as defined by the
+  relationship defined by the 'Dual' instance.
   -}
   parMap4 :: m a0 -> m a1 -> m a2 -> m a3 -> (a0 -> a1 -> a2 -> a3 -> a) -> m a
   parMap4 ma0 ma1 ma2 ma3 f = (#) sequential $ f
@@ -62,10 +76,10 @@ class (Monad m, Applicative f) => Dual f m | m -> f, f -> m where
     <*> (#) parallel ma2
     <*> (#) parallel ma3
 
-  {-
-  It is the analogue to using @<$>@ and @<*>@ for the dual
-  @Applicative@ of the current @Monad@, as defined by the
-  relationship defined by the @Dual@ instance.
+  {- |
+  It is the analogue to using '<$>' and '<*>' for the dual
+  'Applicative' of the current 'Monad', as defined by the
+  relationship defined by the 'Dual' instance.
   -}
   parMap5 :: m a0 -> m a1 -> m a2 -> m a3 -> m a4 -> (a0 -> a1 -> a2 -> a3 -> a4 -> a) -> m a
   parMap5 ma0 ma1 ma2 ma3 ma4 f = (#) sequential $ f
@@ -75,10 +89,10 @@ class (Monad m, Applicative f) => Dual f m | m -> f, f -> m where
     <*> (#) parallel ma3
     <*> (#) parallel ma4
 
-  {-
-  It is the analogue to using @<$>@ and @<*>@ for the dual
-  @Applicative@ of the current @Monad@, as defined by the
-  relationship defined by the @Dual@ instance.
+  {- |
+  It is the analogue to using '<$>' and '<*>' for the dual
+  'Applicative' of the current 'Monad', as defined by the
+  relationship defined by the 'Dual' instance.
   -}
   parMap6 :: m a0 -> m a1 -> m a2 -> m a3 -> m a4 -> m a5 -> (a0 -> a1 -> a2 -> a3 -> a4 -> a5 -> a) -> m a
   parMap6 ma0 ma1 ma2 ma3 ma4 ma5 f = (#) sequential $ f
@@ -89,9 +103,9 @@ class (Monad m, Applicative f) => Dual f m | m -> f, f -> m where
     <*> (#) parallel ma4
     <*> (#) parallel ma5
 
-  {-
-  Same as @traverse@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as 'traverse', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parTraverse :: Traversable t => (a -> m b) -> t a -> m (t b)
   parTraverse f ta =
@@ -99,44 +113,44 @@ class (Monad m, Applicative f) => Dual f m | m -> f, f -> m where
         res = sequenceA $ fmap g ta
     in  (#) sequential res
 
-  {-
-  Same as @traverse_@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as 'traverse_', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parTraverse_ :: Traversable t => (a -> m b) -> t a -> m ()
   parTraverse_ f = void . parTraverse f
 
-  {-
-  Same as @sequence@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as 'sequence', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parSequence :: Traversable t => t (m a) -> m (t a)
   parSequence = parTraverse id
 
-  {-
-  Same as @sequence_@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as 'sequence_', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parSequence_ :: Traversable t => t (m a) -> m ()
   parSequence_ = void . parSequence
 
-  {-
-  Same as @*>@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as '*>', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parProductR :: m a -> m b -> m b
   parProductR ma mb = parMap2 ma mb (\_ b -> b)
 
-  {-
-  Same as @<*@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as '<*', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parProductL :: m a -> m b -> m a
   parProductL ma mb = parMap2 ma mb const
 
-  {-
-  Same as @bitraverse@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as 'bitraverse', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parBitraverse :: Bitraversable t => (a -> m c) -> (b -> m d) -> t a b -> m (t c d)
   parBitraverse ma mb tab =
@@ -145,9 +159,9 @@ class (Monad m, Applicative f) => Dual f m | m -> f, f -> m where
         res = bitraverse fa fb tab
     in  (#) sequential res
 
-  {-
-  Same as @bisequence@, except it uses the dual @Applicative@ of
-  the current @Monad@, as defined by the @Dual@ relationship.
+  {- |
+  Same as 'bisequence', except it uses the dual 'Applicative' of
+  the current 'Monad', as defined by the 'Dual' relationship.
   -}
   parBisequence :: Bitraversable t => t (m a) (m b) -> m (t a b)
   parBisequence = parBitraverse id id
