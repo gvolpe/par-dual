c373ab2be05fad393b8b61d095039d9afccda308
Gabriel Volpe <volpegabriel@gmail.com>
2020-04-29 11:25:35 +0200

    replacing natural-transformations with simple universally quantified types

--- src/Control/ParDual/Class.hs
+++ src/Control/ParDual/Class.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FunctionalDependencies, TypeOperators #-}
+{-# LANGUAGE FunctionalDependencies, RankNTypes, TypeOperators #-}
 
 {-|
 Module      : Control.ParDual.Class
@@ -14,9 +14,6 @@ module Control.ParDual.Class where
 
 import           Control.Applicative            ( ZipList(..) )
 import           Control.Concurrent.Async       ( Concurrently(..) )
-import           Control.Natural                ( (:~>)(..)
-                                                , (#)
-                                                )
 import           Data.Bitraversable             ( Bitraversable
                                                 , bitraverse
                                                 )
@@ -37,11 +34,11 @@ It can also be seen as an isomorphism defined at the class level.
 class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   {- | A natural transformation from 'm' to 'f'
   -}
-  parallel :: m :~> f
+  parallel :: forall a . m a -> f a
 
   {- | A natural transformation from 'f' to 'm'
   -}
-  sequential :: f :~> m
+  sequential :: forall a . f a -> m a
 
   {- |
   It is the analogue to using '<$>' and '<*>' for the dual
@@ -49,9 +46,9 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   relationship defined by the 'ParDual' instance.
   -}
   parMap2 :: m a0 -> m a1 -> (a0 -> a1 -> a) -> m a
-  parMap2 ma0 ma1 f = (#) sequential $ f
-    <$> (#) parallel ma0
-    <*> (#) parallel ma1
+  parMap2 ma0 ma1 f = sequential $ f
+    <$> parallel ma0
+    <*> parallel ma1
 
   {- |
   It is the analogue to using '<$>' and '<*>' for the dual
@@ -59,10 +56,10 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   relationship defined by the 'ParDual' instance.
   -}
   parMap3 :: m a0 -> m a1 -> m a2 -> (a0 -> a1 -> a2 -> a) -> m a
-  parMap3 ma0 ma1 ma2 f = (#) sequential $ f
-    <$> (#) parallel ma0
-    <*> (#) parallel ma1
-    <*> (#) parallel ma2
+  parMap3 ma0 ma1 ma2 f = sequential $ f
+    <$> parallel ma0
+    <*> parallel ma1
+    <*> parallel ma2
 
   {- |
   It is the analogue to using '<$>' and '<*>' for the dual
@@ -70,11 +67,11 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   relationship defined by the 'ParDual' instance.
   -}
   parMap4 :: m a0 -> m a1 -> m a2 -> m a3 -> (a0 -> a1 -> a2 -> a3 -> a) -> m a
-  parMap4 ma0 ma1 ma2 ma3 f = (#) sequential $ f
-    <$> (#) parallel ma0
-    <*> (#) parallel ma1
-    <*> (#) parallel ma2
-    <*> (#) parallel ma3
+  parMap4 ma0 ma1 ma2 ma3 f = sequential $ f
+    <$> parallel ma0
+    <*> parallel ma1
+    <*> parallel ma2
+    <*> parallel ma3
 
   {- |
   It is the analogue to using '<$>' and '<*>' for the dual
@@ -82,12 +79,12 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   relationship defined by the 'ParDual' instance.
   -}
   parMap5 :: m a0 -> m a1 -> m a2 -> m a3 -> m a4 -> (a0 -> a1 -> a2 -> a3 -> a4 -> a) -> m a
-  parMap5 ma0 ma1 ma2 ma3 ma4 f = (#) sequential $ f
-    <$> (#) parallel ma0
-    <*> (#) parallel ma1
-    <*> (#) parallel ma2
-    <*> (#) parallel ma3
-    <*> (#) parallel ma4
+  parMap5 ma0 ma1 ma2 ma3 ma4 f = sequential $ f
+    <$> parallel ma0
+    <*> parallel ma1
+    <*> parallel ma2
+    <*> parallel ma3
+    <*> parallel ma4
 
   {- |
   It is the analogue to using '<$>' and '<*>' for the dual
@@ -95,13 +92,13 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   relationship defined by the 'ParDual' instance.
   -}
   parMap6 :: m a0 -> m a1 -> m a2 -> m a3 -> m a4 -> m a5 -> (a0 -> a1 -> a2 -> a3 -> a4 -> a5 -> a) -> m a
-  parMap6 ma0 ma1 ma2 ma3 ma4 ma5 f = (#) sequential $ f
-    <$> (#) parallel ma0
-    <*> (#) parallel ma1
-    <*> (#) parallel ma2
-    <*> (#) parallel ma3
-    <*> (#) parallel ma4
-    <*> (#) parallel ma5
+  parMap6 ma0 ma1 ma2 ma3 ma4 ma5 f = sequential $ f
+    <$> parallel ma0
+    <*> parallel ma1
+    <*> parallel ma2
+    <*> parallel ma3
+    <*> parallel ma4
+    <*> parallel ma5
 
   {- |
   Same as 'traverse', except it uses the dual 'Applicative' of
@@ -109,9 +106,9 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   -}
   parTraverse :: Traversable t => (a -> m b) -> t a -> m (t b)
   parTraverse f ta =
-    let g a = (#) parallel (f a)
+    let g a = parallel (f a)
         res = sequenceA $ fmap g ta
-    in  (#) sequential res
+    in  sequential res
 
   {- |
   Same as 'Data.Foldable.traverse_', except it uses the dual 'Applicative' of
@@ -154,10 +151,10 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
   -}
   parBitraverse :: Bitraversable t => (a -> m c) -> (b -> m d) -> t a b -> m (t c d)
   parBitraverse ma mb tab =
-    let fa  = (\a -> (#) parallel (ma a))
-        fb  = (\b -> (#) parallel (mb b))
+    let fa  = (\a -> parallel (ma a))
+        fb  = (\b -> parallel (mb b))
         res = bitraverse fa fb tab
-    in  (#) sequential res
+    in  sequential res
 
   {- |
   Same as 'Data.Traversable.bisequence', except it uses the dual 'Applicative' of
@@ -169,13 +166,13 @@ class (Monad m, Applicative f) => ParDual f m | m -> f, f -> m where
 --------------------- Instances ----------------------------
 
 instance Semigroup e => ParDual (Validation e) (Either e) where
-  parallel   = NT fromEither
-  sequential = NT toEither
+  parallel   = fromEither
+  sequential = toEither
 
 instance ParDual Concurrently IO where
-  parallel   = NT Concurrently
-  sequential = NT runConcurrently
+  parallel   = Concurrently
+  sequential = runConcurrently
 
 instance ParDual ZipList [] where
-  parallel   = NT ZipList
-  sequential = NT getZipList
+  parallel   = ZipList
+  sequential = getZipList
